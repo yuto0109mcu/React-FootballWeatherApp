f05ccf063e2d2d644445fd77357cbd965634f588
Yuto <yuto0109MCU@gmail.com>
2020-03-12 02:42:04 +0900

    Updates

---
+++ precache-manifest.d5becb438a9a0f22d5a175fd716ba232.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c19b2fdbc7b1c2d5f23759f27daf24",
+    "url": "/React-FootballWeatherApp/index.html"
+  },
+  {
+    "revision": "412aa55825cdc9671c2b",
+    "url": "/React-FootballWeatherApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a82a4448f6d14462c37",
+    "url": "/React-FootballWeatherApp/static/js/2.ce03af84.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/React-FootballWeatherApp/static/js/2.ce03af84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412aa55825cdc9671c2b",
+    "url": "/React-FootballWeatherApp/static/js/main.56b5748e.chunk.js"
+  },
+  {
+    "revision": "8039a91dc386382119e7",
+    "url": "/React-FootballWeatherApp/static/js/runtime-main.dbf1c42e.js"
+  },
+  {
+    "revision": "daba6b50f4874d7f168f903862049dfe",
+    "url": "/React-FootballWeatherApp/static/media/aa.daba6b50.jpg"
+  },
+  {
+    "revision": "5f50a7f4560129870f648be9fd060d72",
+    "url": "/React-FootballWeatherApp/static/media/clear.5f50a7f4.png"
+  },
+  {
+    "revision": "d5a3eaad514350c8b9f25a465db40a03",
+    "url": "/React-FootballWeatherApp/static/media/notgood.d5a3eaad.png"
+  },
+  {
+    "revision": "41784276e1be8567e17b729bf39ca11f",
+    "url": "/React-FootballWeatherApp/static/media/rainy2.41784276.png"
+  },
+  {
+    "revision": "daf7897b46df6bc66f46337ec6a6c851",
+    "url": "/React-FootballWeatherApp/static/media/snow.daf7897b.png"
+  },
+  {
+    "revision": "34ba5aa7cd544de4959e9642b713107c",
+    "url": "/React-FootballWeatherApp/static/media/sunny2.34ba5aa7.png"
+  },
+  {
+    "revision": "ff2568658efd080fa33fd2147638d267",
+    "url": "/React-FootballWeatherApp/static/media/thor.ff256865.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87425475210bf6dec05aede4454e051d.js"
+  "/React-FootballWeatherApp/precache-manifest.d5becb438a9a0f22d5a175fd716ba232.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-FootballWeatherApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
